1431155d98432ea04390d4dba9ea1ccde123e291
duong <aduong88999@gmail.com>
2024-05-02 21:31:46 +0700

    test

--- controller_old/pdf.php
+++ controller_old/pdf.php
@@ -4,7 +4,7 @@ use Dompdf\Dompdf;
 $dompdf = new Dompdf();
 
 
-$html = file_get_contents('../component/index.php') ;
+$html = file_get_contents('../component/receipt.php') ;
 $dompdf->loadHtml($html);
 $dompdf->setPaper('A4','landscape');
 $dompdf->render();
